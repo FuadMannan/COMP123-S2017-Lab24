ef607ca12b474379a39a5546bd2c5174a616562d
FuadMannan <fuad.mannan@gmail.com>
2017-07-13 16:07:06 -0400

    Updated IHasMalice interface

--- COMP123-S2017-Lesson9B/IHasMalice.cs
+++ COMP123-S2017-Lesson9B/IHasMalice.cs
@@ -8,12 +8,14 @@ using System.Threading.Tasks;
  * Name: Fuad Mannan
  * Date: July 13, 2017
  * Description: This is the IHasMalice interface used in the SuperVillain class
- * Version: 0.1 - Created empty IHasMalice
+ * Version: 0.2 - Added Malice property, changed interface scope to public
  */
 
 namespace COMP123_S2017_Lesson9B
 {
-    interface IHasMalice
+    public interface IHasMalice
     {
+        //PUBLIC PROPERTIES
+        int Malice { get; set; }
     }
 }
