83e6ddf7b22427d9f10cf357b70a6ff74435461f
300923068 <300923068@PR-A315-01>
2017-07-13 12:28:28 -0400

    Created base files

---
+++ COMP123-S2017-Lesson9B/IHasMalice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Fuad Mannan
+ * Date: July 13, 2017
+ * Description: This is the IHasMalice interface used in the SuperVillain class
+ * Version: 0.1 - Created empty IHasMalice
+ */
+
+namespace COMP123_S2017_Lesson9B
+{
+    interface IHasMalice
+    {
+    }
+}
---
+++ COMP123-S2017-Lesson9B/SuperVillain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Fuad Mannan
+ * Date: July 13, 2017
+ * Description: This is the SuperVillain class which inherits from the SuperHuman class
+ * Version: 0.1 - Created empty SuperVillain class
+ */
+
+namespace COMP123_S2017_Lesson9B
+{
+    class SuperVillain : SuperHuman, IHasMalice
+    {
+    }
+}
